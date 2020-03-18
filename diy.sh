def692acc18fd79c642ad3d664b6e337bf7c7313
kong850539619 <38160715+kong850539619@users.noreply.github.com>
2020-03-18 11:56:44 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,7 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.5.1/10.10.10.1/g' package/base-files/files/bin/config_generate
 #移除不用软件包
 rm -rf k3screenctrl package/lean
 #添加额外软件包
